beb8a9a62c9be42d7215a18017439dd727cf2f59
Erik Sanchez <erik.sape@gmail.com>
2016-12-16 15:13:55 -0600

    logeo y logout

--- firebase/web-react/src/components/Header.js
+++ firebase/web-react/src/components/Header.js
@@ -1,27 +1,48 @@
 import React from 'react'
 
 export default class extends React.Component {
-  render(){
+  /**
+   * si no esta logeado aparece el boton para logearse
+   * y si esta logeado aparece los datos del usuario
+   */
+  renderLogin(){
     const { auth, provider, user } = this.props
+    if(user!==null){
+      const pic = {
+        backgroundImage:`url(${user.photoURL})`
+      }
+      const { displayName } = user
+      console.log('pic', pic)
+      return (
+        <div id="user-container">
+          <div id="user-pic" style={pic}></div>
+          <div id="user-name">{displayName}</div>
+          <button id="sign-out" className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--white"
+            onClick={()=>auth.signOut()}
+            >
+            Sign-out
+          </button>
+        </div>
+      )
+    } else {
+      return(
+        <div id="user-container">
+          <button id="sign-in" className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--white" onClick={()=>{ auth.signInWithPopup(provider); }}>
+            <i className="material-icons">account_circle</i>Sign-in with Google
+          </button>
+        </div>
+      )
+    }
+  }
+  render(){
+    const { title } = this.props
     return(
       <header className="mdl-layout__header mdl-color-text--white mdl-color--light-blue-700">
         <div className="mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-grid">
           <div className="mdl-layout__header-row mdl-cell mdl-cell--12-col mdl-cell--12-col-tablet mdl-cell--12-col-desktop">
-            <h3><i className="material-icons">chat_bubble_outline</i>{this.props.title}</h3>
+            <h3><i className="material-icons">chat_bubble_outline</i>{title}</h3>
           </div>
-          <div id="user-container">
-            <div hidden id="user-pic"></div>
-            <div hidden id="user-name"></div>
-            <button hidden id="sign-out" className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--white">
-              Sign-out
-            </button>
-            {
-              user === null &&
-              <button id="sign-in" className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-color-text--white" onClick={()=>{ auth.signInWithPopup(provider); }}>
-                <i className="material-icons">account_circle</i>Sign-in with Google
-              </button>
-            }
-          </div>
+            {this.renderLogin()}
         </div>
       </header>
     )
